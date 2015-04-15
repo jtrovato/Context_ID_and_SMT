a65e105251ec21c713d3d71909d63a1dbd82be97
Meyer Kizner <meyer@pre-alpha.com>
2015-04-14 23:35:04 -0400

    So this doesn't quite work but here's what I have

--- extension-1.py
+++ extension-1.py
@@ -3,6 +3,7 @@ from __future__ import division
 import optparse
 import sys
 import subprocess
+from collections import namedtuple
 
 optparser = optparse.OptionParser()
 optparser.add_option("-d", "--data", dest="train", default="wikidata/es/", help="Data filename prefix (default=data)")
@@ -24,10 +25,10 @@ e_file_name = "%s%s" % (opts.output, ".en")
 stop_words = [word.strip() for word in open(opts.stop_file)]
 
 # put each english sentence in file into a list
-e_sents = [english.strip() for english in open(e_data) if len(english.strip()) > 0][:opts.num_sents]
+e_sents = [english.strip().split() for english in open(e_data) if len(english.strip()) > 0][:opts.num_sents]
 
 # put each spanish sentence from file into a list
-s_sents = [spanish.strip() for spanish in open(s_data) if len(spanish.strip()) > 0][:opts.num_sents]
+s_sents = [spanish.strip().split() for spanish in open(s_data) if len(spanish.strip()) > 0][:opts.num_sents]
 
 # put the dictionary entries into a list
 es_lists = [line.strip().split() for line in open(opts.esdict)]
@@ -38,38 +39,71 @@ es_map = {}
 for es_line in es_lists:
     es_map[es_line[0]] = es_line[1:]
 
+def bitmap(sequence):
+    """ Generate a coverage bitmap for a sequence of indexes """
+    return reduce(lambda x,y: x|y, map(lambda i: long('1'+'0'*i,2), sequence), 0)
+
+phrase = namedtuple("phrase", "english, logprob")
+def TM(filename, k):
+    tm = {}
+    for line in open(filename).readlines():
+        (f, e, logprob) = line.strip().split(" ||| ")
+        tm.setdefault(tuple(f.split()), []).append(phrase(e.strip(), float(logprob)))
+    for f in tm: # prune all but top k translations
+        tm[f].sort(key=lambda x: -x.logprob)
+        del tm[f][k:]
+    for word in set(sum(map(lambda x: tuple(x), s_sents),())):
+        if (word,) not in tm:
+            tm[(word,)] = [phrase(word, 0.0)]
+    return tm
+
+def find_alignments(tm, e, f):
+    alignments = [[] for _ in e]
+    for fi in xrange(len(f)):
+        for fj in xrange(fi+1,len(f)+1):
+            if f[fi:fj] in tm:
+                for phrase in tm[f[fi:fj]]:
+                    ephrase = tuple(phrase.english.split())
+                    for ei in xrange(len(e)+1-len(ephrase)):
+                        ej = ei+len(ephrase)
+                        if ephrase == e[ei:ej]:
+                            alignments[ei].append((ej, phrase.logprob, fi, fj))
+
+def score_translation(tm, e, f, alignments):
+    chart = [{} for _ in e] + [{}]
+    chart[0][0] = 0.0
+    for ei, sums in enumerate(chart[:-1]):
+        for v in sums:
+            for ej, logprob, fi, fj in alignments[ei]:
+                if bitmap(range(fi,fj)) & v == 0:
+                    new_v = bitmap(range(fi,fj)) | v
+                    if new_v in chart[ej]:
+                        chart[ej][new_v] = logadd10(chart[ej][new_v], sums[v]+logprob)
+                    else:
+                        chart[ej][new_v] = sums[v]+logprob
+    goal = bitmap(range(len(f)))
+    if goal in chart[len(e)]:
+        return chart[len(e)][goal]
+    else:
+        return None
+
 def align():
     # make lists for the aligned sentences that we will output
     e_output = []
     s_output = []
-    for eindex, e in enumerate(e_sents):
-        # only look at spanish sentences within the distance window
-        start = max(0, eindex - opts.win_size)
-        end = min(len(s_sents), eindex + opts.win_size)
-        
-        # keep track of best aligned score and sentence
-        best_s = ''
+    tm = TM('tm', 50)
+    for i, s_sent in enumerate(s_sents):
         best_score = 0.0
-        for s in s_sents[start:end]:
-            count_overlap = 0
-            # for this spanish sentence, count the number of spanish words
-            # that have a dictionary translation in the english sentence
-            for s_word in s.split():
-                if s_word in es_map:
-                    translations = es_map[s_word]
-                    for e_word in e.split():
-                        if e_word in translations and e_word not in stop_words:
-                            count_overlap += 1
-                            break
-            # give the sentence a score normalized for length of the sentence
-            score = float(count_overlap)/len(s)
-            if score > best_score:
-                best_score = score
-                best_s = s
-        # append each sentence if best alignment above thresh
-        if best_score > opts.threshold:
-            e_output.append(e)
-            s_output.append(best_s)
+        best_sent = None
+        for e_sent in e_sents[i-opts.win_size:i+opts.win_size]:
+            alignments = find_alignments(tm, tuple(e_sent), tuple(s_sent))
+            if alignments:
+                score = score_translation(tm, e_sent, s_sent, alignments)
+                if score > best_score:
+                    best_sent = e_sent
+        if best_sent:
+            e_output.append(best_sent.join(" "))
+            s_output.append(s_sent.join(" "))
     return (e_output, s_output)
 
 def align_and_print():
